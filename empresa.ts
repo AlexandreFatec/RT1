cab471bad526d7a9c7ad759ac5d547440636925c
Alexandre Ribeiro <160600262+AlexandreFatec@users.noreply.github.com>
2025-03-13 10:40:51 -0300

    Update empresa.ts

--- empresa.ts
+++ empresa.ts
@@ -1,13 +1,22 @@
+import Endereco from "./endereco";
+import Funcionario from "./funcionario";
+import Telefone from "./telefone";
+
 export default class Empresa {
     public razaoSocial: string;
     public nomeFantasia: string;
     public cnpj: string;
+    public endereco: Endereco;
+    public funcionarios: Funcionario[];
+    public telefones: Telefone[];
  
-    constructor(razaoSocial: string, nomeFantasia: string, cnpj: string) {
+    constructor(funcionarios: Funcionario[], endereco: Endereco, razaoSocial: string, nomeFantasia: string, cnpj: string, telefones: Telefone[]) {
+        this.funcionarios = funcionarios;
+        this.endereco = endereco;
         this.razaoSocial = razaoSocial;
         this.nomeFantasia = nomeFantasia;  
         this.cnpj = cnpj;
+        this.telefones = telefones;
     }
 
 }
-
