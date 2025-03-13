a02c84519a224c8b596170aaa751ed0c28396e80
Alexandre Ribeiro <160600262+AlexandreFatec@users.noreply.github.com>
2025-03-13 10:42:43 -0300

    Update index.ts

--- index.ts
+++ index.ts
@@ -1,3 +1,4 @@
+import Descritor from './descritorEmpresa'
 import Empresa from './empresa'
 import Endereco from './endereco'
 import Funcionario from './funcionario'
@@ -5,11 +6,12 @@ import Telefone from './telefone'
 
 let endereco = new Endereco(123,`Av Paulista`,`Jd Paulista`,`SP`)
 let telefone = new Telefone(`11`,`12345678`)
-let funcionario = new Funcionario(`Tony Stark`,`12345678`,`365.355.252-00`)
-let empresa = new Empresa(`ABC LTDA`,`Mercado Online`,`999.999.999-99`)
+let funcionario = new Funcionario(`Tony Stark`,`12345678`,`365.355.252-00`, endereco, telefone)
+let funcionario2 = new Funcionario(`Steve Rogers`, `23456789`, `234.567.890-11`, endereco, telefone);
+let funcionario3 = new Funcionario(`Natasha Romanoff`, `34567890`, `345.678.901-22`, endereco, telefone);
+let funcionarios = [funcionario, funcionario2, funcionario3]
+let telefones = [telefone]
+let empresa = new Empresa(funcionarios, endereco, `ABC LTDA`,`Mercado Online`,`999.999.999-99`, telefones)
 
-
-console.log(endereco.mostrarEndereco())
-
-
-
+let descritor = new Descritor();
+descritor.descrever(empresa);
